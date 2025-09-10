a819e767f20cf2a155bb7320e298aac778471df4
asukas00 <jessere2025@gmail.com>
2025-09-10 07:18:11 -0700

    saves

--- test.py
+++ test.py
@@ -2,12 +2,53 @@ import turtle
 from turtle import *
 t = Turtle()
 
+""" def message(hello):
+    print(hello)
+message("Hello Class") """
 
 t.shape('turtle')
 
-t.forward(200)
+""" def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200) """
 
-def message(input):
-    print(input)
-    message("Hello Class")
-turtle.done()
+""" def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200) """
+
+""" def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right() """
+
+""" def rectangle(x):
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+rectangle(200) """
+def equal(x):
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+equal(50) 
+turtle.done
