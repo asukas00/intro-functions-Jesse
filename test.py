f39525d24afb8f3d4559c88b7b832fd6d21d9df7
asukas00 <jessere2025@gmail.com>
2025-09-11 07:32:22 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import turtle
 from turtle import *
 t = Turtle()
+t.speed(10)
 
-""" def message(hello):
-    print(hello)
+""" def message(input):
+    print(input)
 message("Hello Class") """
 
 t.shape('turtle')
@@ -57,10 +58,45 @@ equal(50)  """
     t.left(90) """
 #answers for loop.md
 
-sidelength = 100
-rotate = 90
-def square(x,y):
+
+""" def triangle(x):
+    for i in range(3):
+        t.forward(x)
+        t.left(120)
+    
+
+def tri():
+    size = 50
+    for i in range(60):
+        triangle(size)
+        size += 5
+tri() """
+
+def square(x):
     for i in range(4):
         t.forward(x)
-        t.left(y)
+        t.left(90)
+
+""" def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 1.5
+doubleSquares(5)
+
+def addsquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length += 25 
+addsquares(5)   """
+
+for square in range(60):
+    size = 40
+    square(size)
+    size += 10
+
+
+
+
 turtle.done
